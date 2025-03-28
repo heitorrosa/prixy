c10080e927474e45233995514de841bb4854423f
heitorrosa <heitorolivrosa@gmail.com>
2025-03-28 20:18:25 -0300

    feat: adicionado conexao para banco de dados MySQL hosteado via XAMPP

--- back-end/prixy.js
+++ back-end/prixy.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const mySQL = require("mysql");
 
 const prixy = express();
 
+// Configura a conexão com o banco de dados MySQL (XAMPP)
+const bancoDados = mySQL.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "sistemalogin-nodejs"
+});
+
+// Conecta ao banco de dados MySQL (XAMPP)
+bancoDados.connect((err) => {
+    if (err) {
+        console.error("Erro ao conectar ao banco de dados:", err);
+        return;
+    }
+    console.log("Conexão com o banco de dados estabelecida com sucesso!");
+});
+
+
 prixy.get("/", (req, res) => {
-    res.send("<a>Pong!</a>")
+    res.send("<a>Sucesso!</a>")
 })
 
 prixy.listen(5000, () => {
